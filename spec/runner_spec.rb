c8bb0252e7c2f2bab6c7e81bc202fcc1f56f24b4
Trevor John and Yuki Nishijima <pair+tjohn+ynishijima@pivotallabs.com>
2014-10-28 14:00:44 -0400

    do not throw error when response data is nil
    - does not handle response nil case since faraday response will never be nil

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -16,29 +16,17 @@ describe PlayTime::Runner do
       it 'returns a response' do
         expect(subject).to eq response
       end
-    end
-
-    context 'when failure' do
-      context 'when no response' do
-        let(:response) { nil }
-
-        it 'raises an error' do
-          expect {
-            subject
-          }.to raise_error PlayTime::Runner::IOError
-        end
-      end
 
       context 'when data is nil' do
         let(:response) { double(:response, data: nil) }
 
-        it 'raises an error' do
-          expect {
-            subject
-          }.to raise_error PlayTime::Runner::IOError
+        it 'returns the response' do
+          expect(subject).to eq response
         end
       end
+    end
 
+    context 'when failure' do
       context 'when data has an error' do
         let(:response) { double(:response, data: {'error' => 'Something went wrong'}) }
 
