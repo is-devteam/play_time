f61a0c6b679c67b5ee4d5c74de9839e309d7d509
Trevor John <pair+tjohn@pivotallabs.com>
2014-10-29 10:49:53 -0400

    find most recent file in path when loading apk

--- lib/play_time/apk.rb
+++ lib/play_time/apk.rb
@@ -2,17 +2,32 @@ require 'google/api_client'
 
 module PlayTime
   class Apk
+    class FileNotFound < StandardError; end
+
     MIME_TYPE = 'application/vnd.android.package-archive'.freeze
 
     class << self
       def load
-        Google::APIClient::UploadIO.new(path, MIME_TYPE)
+        Google::APIClient::UploadIO.new(most_recent_modified_file_path, MIME_TYPE)
       end
 
       private
 
-      def path
+      def file_path
         PlayTime.configuration.apk_path
+      end
+
+      def most_recent_modified_file_path
+        @most_recent_modified_file_path ||=
+          begin
+            file = Dir[file_path].map{|file_path| File.open(file_path) }.sort_by(&:mtime).last
+
+            if file
+              file.path
+            else
+              raise FileNotFound, "No such file or directory: #{file_path}"
+            end
+          end
       end
     end
   end
--- spec/lib/apk_spec.rb
+++ spec/lib/apk_spec.rb
@@ -1,11 +1,16 @@
 describe PlayTime::Apk do
   describe '#load' do
-    let(:path) { 'path/to/apk' }
+    let(:path) { 'path/to/*apk' }
+    let(:old_file) { instance_double(File, mtime: Time.now - 10, path: 'should not get here') }
+    let(:file) { instance_double(File, mtime: Time.now, path: 'newest/file') }
 
     subject { PlayTime::Apk.load }
 
     before do
       allow(PlayTime.configuration).to receive(:apk_path).and_return(path)
+      allow(File).to receive(:open).with(file.path).and_return(file)
+      allow(File).to receive(:open).with(old_file.path).and_return(old_file)
+      allow(Dir).to receive(:[]).and_return([file.path, old_file.path])
     end
 
     it 'returns a Google::APIClient::UploadIO with the path and MIME_TYPE' do
@@ -13,7 +18,29 @@ describe PlayTime::Apk do
 
       subject
 
-      expect(Google::APIClient::UploadIO).to have_received(:new).with(path, PlayTime::Apk::MIME_TYPE)
+      expect(Google::APIClient::UploadIO).to have_received(:new).with('newest/file', PlayTime::Apk::MIME_TYPE)
+    end
+
+    it "searches the directory for the path" do
+      subject
+
+      expect(Dir).to have_received(:[]).with path
+    end
+
+    context 'when file not found' do
+      before do
+        allow(Dir).to receive(:[]).and_return([])
+      end
+
+      it 'raises an error' do
+        expect {
+          subject
+        }.to raise_error(PlayTime::Apk::FileNotFound, "No such file or directory: #{path}")
+      end
+    end
+
+    after do
+      PlayTime::Apk.instance_variable_set(:@most_recent_modified_file_path, nil)
     end
   end
 end
